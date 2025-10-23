33039e97f41fe32afe64926493faa591f58232bd
youcef23061983 <youcef.belcourtois@gmail.com>
2025-10-23 16:26:40 +0100

    commit12

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,10 +1,8 @@
 import type { Config } from "@react-router/dev/config";
-import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  presets: [vercelPreset()],
 
   ssr: true,
 } satisfies Config;
