208a421274fd6cea629d624248586364db73cdea
youcef23061983 <youcef.belcourtois@gmail.com>
2025-11-26 17:10:05 +0100

    google analytics commit

--- app/root.tsx
+++ app/root.tsx
@@ -31,6 +31,21 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-77LPYQR028`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-77LPYQR028');
+      `,
+          }}
+        />
 
         {/* ✅ ADD GOOGLE SITE VERIFICATION HERE */}
         <meta
