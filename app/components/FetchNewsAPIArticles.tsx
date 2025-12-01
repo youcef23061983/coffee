78576907cb098e2cc53e3308179f996bf533d89a
youcef23061983 <youcef.belcourtois@gmail.com>
2025-12-01 17:12:13 +0100

    fix name commit

--- app/components/FetchNewsAPIArticles.tsx
+++ app/components/FetchNewsAPIArticles.tsx
@@ -12,7 +12,7 @@ interface CoffeeArticle {
 }
 
 // REMOVED "async" from function name - this was the problem!
-export default function FetchNewsAPIArticles() {
+export default function FetchnewsApiarticles() {
   const [articles, setArticles] = useState<CoffeeArticle[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
--- app/components/welcome.tsx
+++ app/components/welcome.tsx
@@ -5,7 +5,7 @@ import { ClientOnly } from "./ClientOnly";
 import RealCoffeeCategories, {
   CategoriesLoadingSkeleton,
 } from "./RealCoffeeCategories";
-import FetchNewsAPIArticles from "./fetchNewsAPIArticles";
+import FetchnewsApiarticles from "./FetchNewsAPIArticles";
 
 interface WelcomeProps {
   data: any[]; // or use a more specific type
@@ -281,7 +281,7 @@ export function Welcome({ data, brands }: WelcomeProps) {
         </div>
       </section>
 
-      <FetchNewsAPIArticles />
+      <FetchnewsApiarticles />
 
       <section className="py-20 bg-linear-to-br from-[#8B4513] to-[#6B3410] text-white">
         <div className="max-w-4xl mx-auto px-4 text-center">
