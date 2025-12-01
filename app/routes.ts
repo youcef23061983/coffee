ba34bc70b595903cacb13ff090fcb1e21501929e
youcef23061983 <youcef.belcourtois@gmail.com>
2025-12-01 17:04:33 +0100

    reverse

--- app/routes.ts
+++ app/routes.ts
@@ -8,9 +8,8 @@ import {
 export default [
   layout("routes/__root.tsx", [
     index("routes/home.tsx"),
-    route("/:brandId", "routes/brands.tsx"),
-
     route("sitemap.xml", "routes/sitemap[.]xml.tsx"),
+    route("/:brandId", "routes/brands.tsx"),
 
     route("products", "routes/products.tsx"),
     route("products/:id", "routes/detail.tsx"),
