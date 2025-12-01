28b7014e419c82bb71113e3af931c124c8032c64
youcef23061983 <youcef.belcourtois@gmail.com>
2025-12-01 19:35:23 +0100

    client

--- app/components/welcome.tsx
+++ app/components/welcome.tsx
@@ -279,8 +279,9 @@ export function Welcome({ data, brands }: WelcomeProps) {
           <TestimonialsSection />
         </div>
       </section>
-
-      <FetchNewsAPIArticles />
+      <ClientOnly fallback={<CategoriesLoadingSkeleton />}>
+        <FetchNewsAPIArticles />
+      </ClientOnly>
 
       <section className="py-20 bg-linear-to-br from-[#8B4513] to-[#6B3410] text-white">
         <div className="max-w-4xl mx-auto px-4 text-center">
